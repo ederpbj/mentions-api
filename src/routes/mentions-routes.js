e3a2a2919bbff6eea584ba4bea5e7bdea8d42419
ederpbj <ederpbj@yahoo.com.br>
2019-10-31 21:00:03 -0300

    atualização readme

--- src/routes/mentions-routes.js
+++ src/routes/mentions-routes.js
@@ -10,7 +10,7 @@ const { check } = require('express-validator');
 router.get('/', mentionsController.listMentions);
 
 router.post('/', [
-    check('friend').isLength({ min: 7 }).withMessage("O nome precisa ter no mínimo 7 caracteres."),
+    check('friend').isLength({ min: 4 }).withMessage("O nome precisa ter no mínimo 4 caracteres."),
     check('mention').isLength({ min: 20, max: 280 }).withMessage("A menção precisa ter no mínimo 20 caracteres e no máximo 280.")
 ], mentionsController.createMention);
 
